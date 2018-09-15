37786c00edde185987f588d7217ea5282f40b74d
Alexander Balan <fantom141141@gmail.com>
2018-09-16 00:37:30 +0300

    Implements quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const a = eval(equation.match(/\-?\s*\d+\s*\*?\s*x\s*\^\s*2/)[0].match(/\-?\s*\d+/)[0]);
+    const b = eval(equation.match(/[\-?\+?]\s*\d+\s*\*?\s*x[^\^]/)[0].match(/\-?\s*\d+/)[0]);
+    const c = eval(equation.match(/[\-?\+?]\s*\d+$/)[0].match(/\-?\s*\d+/)[0]);
+
+    const D = Math.pow(b, 2) - 4 * a * c;
+
+    const x1 = Math.round((-b - Math.pow(D, 0.5)) / (2 * a));
+    const x2 = Math.round((-b + Math.pow(D, 0.5)) / (2 * a));
+
+    return [x1, x2].sort((a, b) => a - b);
 }
